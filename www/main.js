66d7ed9f3a56ba07fd87902a1b7fc5e0c47bc400
kabegin <kabegin@gmail.com>
2016-12-25 13:33:08 +0900

    122ページ

--- www/main.js
+++ www/main.js
@@ -1,10 +1,20 @@
 // This is a JavaScript file
 function doClick(){
-   var data=new Date();
-   var n=document.getElementById('text1').value;
-   var time=data.getTime();
-   var result=time+(60*60*24*1000)*n;
-   var data2=new Date(result);
-   document.getElementById('msg').textContent=data2.toLocaleDateString();
+   var date1=new Date();
+   var str=document.getElementById('text1').value;
+   var date2=new Date(str);
+   date1.setMilliseconds(0);
+   date1.setSeconds(0);
+   date1.setMinutes(0);
+   date1.setHours(0);
+   date2.setMilliseconds(0);
+   date2.setSeconds(0);
+   date2.setMinutes(0);
+   date2.setHours(0);
+   var time1=date1.getTime();
+   var time2=date2.getTime();
+   var result=(time2-time1)/(60*60*24*1000);
+   
+   document.getElementById('msg').textContent=result+'日間あります。';
 }
 
